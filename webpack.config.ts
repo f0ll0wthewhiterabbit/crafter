ffd0412033273f98ac54bcbdcd52f4007cb00d7d
f0ll0wthewhiterabbit <3tonishere@gmail.com>
2021-01-01 21:32:00 +0300

    Introduced webpack

---
+++ webpack.config.ts
@@ -0,0 +1,68 @@
+import path from 'path'
+import webpack, { Configuration } from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import InterpolateHtmlPlugin from 'react-dev-utils/InterpolateHtmlPlugin'
+
+const webpackConfig = (env): Configuration => ({
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'build.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+        exclude: /dist/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        // ?name=[name].[ext] is only necessary to preserve the original file name
+        use: ['file-loader?name=[name].[ext]'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+    }),
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, {
+      PUBLIC_URL: env.production ? '' : 'public/',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html', '**/favicon.ico'],
+          },
+        },
+      ],
+    }),
+    new webpack.DefinePlugin({
+      'process.env.PRODUCTION': env.production || !env.development,
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      eslint: {
+        // same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
+        files: './src/**/*.{ts,tsx,js,jsx}',
+      },
+    }),
+  ],
+})
+
+export default webpackConfig
