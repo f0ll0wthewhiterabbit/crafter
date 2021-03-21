0679855c8528ddd25753f9e19c8c3a71ad702e67
f0ll0wthewhiterabbit <3tonishere@gmail.com>
2021-03-21 15:27:08 +0300

    Implemented redirect from ErrorPage on log out

--- client/src/pages/Error/ErrorPage.tsx
+++ client/src/pages/Error/ErrorPage.tsx
@@ -1,14 +1,23 @@
 import React, { FC } from 'react'
+import { useSelector } from 'react-redux'
+import { Redirect, useHistory } from 'react-router-dom'
 import { Result, Button } from 'antd'
-import { useHistory } from 'react-router-dom'
+
+import { RootState } from '@/store/rootReducer'
 
 import { ROUTE_NAMES } from '@/router/routes.constants'
 
 const ErrorPage: FC = () => {
+  const { isAuthenticated } = useSelector((state: RootState) => state.auth)
+  const { isInitialized } = useSelector((state: RootState) => state.auth)
   const history = useHistory()
 
   const redirectToHomePage = () => {
     history.push(ROUTE_NAMES.HOME)
+  }
+
+  if (!isAuthenticated && isInitialized) {
+    return <Redirect to={ROUTE_NAMES.SIGN_IN} />
   }
 
   return (
